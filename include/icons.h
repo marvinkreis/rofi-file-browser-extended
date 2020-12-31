39cb8232cfd2a61e11ce826c78a4789daf1f44d5
Marvin Kreis <MarvinKreis@web.de>
2020-12-31 18:21:27 +0100

    Add some comments

--- include/icons.h
+++ include/icons.h
@@ -5,8 +5,19 @@
 
 #include "types.h"
 
+/**
+ * Destroys the icon data.
+ */
 void destroy_icon_data ( FileBrowserIconData *id );
+
+/**
+ * Requests icons for the file from rofi's icon fetcher.
+ */
 void request_icons_for_file ( FBFile *fbfile, int icon_size, FileBrowserIconData *id );
+
+/**
+ * Fetches requested icons for the file from rofi's icon fetcher.
+ */
 cairo_surface_t *fetch_icon_for_file ( FBFile *fbfile );
 
 #endif
